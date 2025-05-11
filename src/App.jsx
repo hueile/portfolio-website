a1b02f26f2dbe5091e95d054c41163c2ec34dcd0
hueile <gle@lakeheadu.ca>
2025-05-11 17:44:46 -0400

    added some camera controls (commented out)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import Room from './Room';
+import * as THREE from 'three';
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 5, 0] }}>
+    <Canvas camera={{ position: [0, 5, 0], fov: 50 }}>
       <ambientLight intensity={1} />
-      <directionalLight position={[0, 0, 6.0]}
-       />
+      <directionalLight position={[0, 0, 6.0]} />
       <Room />
-      <OrbitControls />
+      <OrbitControls
+        // enablePan={false}
+        // target={[0, 1, 0]}
+        // mouseButtons={{
+        //   LEFT: null,
+        //   MIDDLE: THREE.MOUSE.ROTATE,
+        //   RIGHT: THREE.MOUSE.ROTATE
+        // }}
+        // minDistance={1}
+        // maxDistance={8}
+       />
     </Canvas>
   );
 }
