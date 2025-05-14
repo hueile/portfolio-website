a249268a537d2c1be7a586f79e47663285b175a7
hueile <gle@lakeheadu.ca>
2025-05-14 01:57:11 -0400

    upgraded the camera controls

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import * as THREE from 'three';
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 5, 0], fov: 50 }}>
+    <Canvas camera={{ position: [0, 5, 0], fov: 50}}>
       <ambientLight intensity={1} />
       <directionalLight position={[0, 0, 6.0]} />
       <Room />
       <OrbitControls
-        // enablePan={false}
-        // target={[0, 1, 0]}
-        // mouseButtons={{
-        //   LEFT: null,
-        //   MIDDLE: THREE.MOUSE.ROTATE,
-        //   RIGHT: THREE.MOUSE.ROTATE
-        // }}
-        // minDistance={1}
-        // maxDistance={8}
+        enablePan={false}
+        target={[0, 2, 0]}
+        mouseButtons={{
+          LEFT: null,
+          MIDDLE: THREE.MOUSE.ROTATE,
+          RIGHT: THREE.MOUSE.ROTATE
+        }}
+        minDistance={1}
+        maxDistance={8}
        />
     </Canvas>
   );
